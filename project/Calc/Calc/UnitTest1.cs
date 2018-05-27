9761db4871ea738eb1879ed60463337562e2cc2b
CnupTyc <cnuptyc543@gmail.com>
2018-05-27 21:30:43 +0700

    Тест суммирования

--- project/Calc/Calc/UnitTest1.cs
+++ project/Calc/Calc/UnitTest1.cs
@@ -6,9 +6,19 @@ namespace Calc
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        ///Получает или устанавливает контекст теста, в котором предоставляются
+        ///сведения о текущем тестовом запуске и обеспечивается его функциональность.
+        ///</summary>
         [TestMethod]
-        public void TestMethod1()
+        public void TestSum()
         {
+            Calculator m = new Calculator();
+            double a = 5;
+            double b = 6;
+            double c = 11;
+            double res = m.Sum(a, b);
+            Assert.AreEqual(res.ToString("0.000"), c.ToString("0.000"));
         }
     }
 }
